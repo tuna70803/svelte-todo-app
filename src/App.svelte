c36e1185a0f8c6295a6dde8ff3a106c577686956
Tuna <tuna70803@gmail.com>
2022-03-19 22:28:45 +0900

    메인 앱 변경 및 Floater 컴포넌트 추가
    Todo 앱을 구성할 수 있도록 메인 앱 컴포넌트를 수정한다.
    배경 이미지를 설정하고 가운데 Floater를 띄워서 Todo 목록을 표시할 수 있도록
    만든다. 이미지 파일은 public 폴더에 추가해야만 로드할 수 있는 것 같아서 해당
    폴더에 추가했다. Floater는 내용없이 css만 설정했다.

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,33 @@
-<main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte
-    apps.
-  </p>
-  <div class="scss-test">
-    <span>with scss!</span>
-  </div>
+<main class="app">
+  <Floater />
 </main>
 
 <script lang="ts">
-  export let name: string;
+  import Floater from './views/Floater.svelte';
 </script>
 
 <style lang="scss">
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
+  .app {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: row;
+    flex: 1 1 auto;
+    justify-content: center;
+    align-items: center;
+    position: relative;
   }
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  .scss-test {
-    font-size: 20px;
-    color: yellowgreen;
-
-    & > span {
-      font-size: 44px;
-    }
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+  .app::after {
+    content: "";
+    background: url('../images/background.jpg');
+    background-size: cover;
+    opacity: 0.1;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    z-index: -1;
   }
 </style>
---
+++ src/views/Floater.svelte
@@ -0,0 +1,15 @@
+<div class="floater"></div>
+
+<style lang="scss">
+  .floater {
+    width: 70%;
+    max-width: 1024px;
+    height: 70%;
+    display: flex;
+    flex-direction: column;
+    border-radius: 5px;
+    background-color: white;
+    box-shadow: 0 30px 50px rgba(0, 0, 0, 0.2);
+    position: relative;
+  }
+</style>
